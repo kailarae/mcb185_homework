ab3a5ae91b329a3d28012d365fd31af945e3aaa9
kailarae <kailaraeann03@gmail.com>
2025-03-15 01:01:42 -0700

    unit4

---
+++ 42ntcomp.py
@@ -0,0 +1,81 @@
+import mcb185
+import sys
+
+# gc content
+#for defline, seq in mcb185.read_fasta(sys.argv[1]):
+    #defwords = defline.split()
+    #name = defwords[0]
+    #gc = 0
+    #for nt in seq:
+        #if nt == 'C' or nt == 'G': gc += 1
+    #print(name, gc/len(seq))
+
+# individual variables
+#for defline, seq in mcb185.read_fasta(sys.argv[1]):
+    #defwords = defline.split()
+    #name = defwords[0]
+    #A = 0
+    #C = 0
+    #G = 0
+    #T = 0
+    #for nt in seq:
+        #if nt == 'A': A += 1
+        #elif nt == 'C': C += 1
+        #elif nt == 'G': G += 1
+        #elif nt == 'T': T += 1
+        #else: N += 1
+    #print(name, A/len(seq), G/len(seq), T/len(seq), C/len(seq))
+
+# list variation
+#for defline, seq in mcb185.read_fasta(sys.argv[1]):
+    #defwords = defline.split()
+    #name = defwords[0]
+    #counts = [0, 0, 0, 0, 0] # A, C, G, T, N
+    #for nt in seq:
+        #if nt == 'A': counts[0] += 1
+        #elif nt == 'C': counts[1] += 1
+        #elif nt == 'G': counts[2] += 1
+        #elif nt == 'T': counts[3] += 1
+        #else: counts[4] += 1
+    #print(name, end='')
+    #for n in counts: print(n/len(seq), end='  ')
+    #print()
+
+# indexing with str.find()
+#for defline, seq in mcb185.read_fasta(sys.argv[1]):
+    #defwords = defline.split()
+    #name = defwords[0]
+    #nts = 'ACGTN'
+    #counts = [0] * len(nts)         # shortcut for assigning zeroes
+    #for nt in seq:
+        #idx = nts.find(nt)          # if the nt is found, its index is returned
+        #counts[idx] += 1
+    #print(name, end='  ')
+    #for n in counts: print(n/len(seq), end='   ')
+    #print()
+
+# counting any letter
+#for defline, seq in mcb185.read_fasta(sys.argv[1]):
+    #defwords = defline.split()
+    #name = defwords[0]
+    #nts = []
+    #counts = []
+    #for nt in seq: 
+        #if nt not in nts:       # finds new letters and adds to nts
+            #nts.append(nt)
+            #counts.append(0)
+        #idx = nts.index(nt)
+        #counts[idx] += 1
+    #print(name)
+    #for nt, n in zip(nts, counts):
+        #print(nt, n, n/len(seq))
+    #print()
+
+# counting with str.count()
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+    defwords = defline.split()
+    name = defwords[0]
+    print(name, end='  ')
+    for nt in 'ACGTN':
+        print(seq.count(nt) / len(seq), end='  ')
+    print()
